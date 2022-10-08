5208055990a95468bfdf1ef7c617dd0028667bef
mancopp <mancoppor@gmail.com>
2022-10-08 16:36:30 +0200

    Main styles

--- src/App.js
+++ src/App.js
@@ -2,7 +2,29 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
+    <div className="calculator-grid">
+      <div className="output">
+        <div className="prev-operand">144*</div>
+        <div className="сurr-operand">434329</div>
+      </div>
+      <button className='span-two'>AC</button>
+      <button>Del</button>
+      <button>/</button>
+      <button>1</button>
+      <button>2</button>
+      <button>3</button>
+      <button>*</button>
+      <button>4</button>
+      <button>5</button>
+      <button>6</button>
+      <button>+</button>
+      <button>7</button>
+      <button>8</button>
+      <button>9</button>
+      <button>-</button>
+      <button>0</button>
+      <button>.</button>
+      <button className='span-two'>=</button>
     </div>
   );
 }
