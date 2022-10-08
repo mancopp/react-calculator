6158a9ce72b221267018a93ce586b94d4e4b09fd
mancopp <mancoppor@gmail.com>
2022-10-08 19:18:05 +0200

    Added digit buttons handling

--- src/App.js
+++ src/App.js
@@ -1,29 +1,49 @@
+import { useReducer } from "react";
 import './App.css';
+import DigitButton from "./components/DigitButton";
+
+export const ACTIONS = {
+  ADD_DIGIT: "add-digit"
+}
+
+function reducer(state, { type, payload }) {
+  switch (type) {
+    case ACTIONS.ADD_DIGIT:
+      return {
+        ...state,
+        currOperand: `${state.currOperand || ""}${payload}`
+      };
+    default:
+      return state;
+  }
+}
 
 function App() {
+  const [{ prevOperand, currOperand, operation }, dispatch] = useReducer(reducer, {});
+
   return (
     <div className="calculator-grid">
       <div className="output">
-        <div className="prev-operand">144*</div>
-        <div className="сurr-operand">434329</div>
+        <div className="prev-operand">{prevOperand}</div>
+        <div className="сurr-operand">{currOperand}</div>
       </div>
       <button className='span-two'>AC</button>
       <button>Del</button>
       <button>/</button>
-      <button>1</button>
-      <button>2</button>
-      <button>3</button>
+      <DigitButton value="1" dispatch={dispatch} />
+      <DigitButton value="2" dispatch={dispatch} />
+      <DigitButton value="3" dispatch={dispatch} />
       <button>*</button>
-      <button>4</button>
-      <button>5</button>
-      <button>6</button>
+      <DigitButton value="4" dispatch={dispatch} />
+      <DigitButton value="5" dispatch={dispatch} />
+      <DigitButton value="6" dispatch={dispatch} />
       <button>+</button>
-      <button>7</button>
-      <button>8</button>
-      <button>9</button>
+      <DigitButton value="7" dispatch={dispatch} />
+      <DigitButton value="8" dispatch={dispatch} />
+      <DigitButton value="9" dispatch={dispatch} />
       <button>-</button>
-      <button>0</button>
-      <button>.</button>
+      <DigitButton value="0" dispatch={dispatch} />
+      <DigitButton value="." dispatch={dispatch} />
       <button className='span-two'>=</button>
     </div>
   );
---
+++ src/components/DigitButton.js
@@ -0,0 +1,11 @@
+import { ACTIONS } from "../App";
+
+function DigitButton({ value, dispatch }) {
+    return (
+        <button onClick={() => dispatch({ type: ACTIONS.ADD_DIGIT, payload: value })}>
+            {value}
+        </button>
+    );
+}
+
+export default DigitButton;
